2074f5110e53424ad90f8fa422be56cdf1693d44
Sowmya Ari <sowmya.ari@everest.engineering>
2021-04-25 22:33:28 +0530

    Add funtionality for calculating total price with given quantity and item price to pass unit test

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -2,6 +2,7 @@
 public class PriceCalculator {
 
     public float calculatePriceWithoutTax(int quantity, float itemPrice){
+        return quantity * itemPrice;
     }
 
     public static void main(String[] args){
