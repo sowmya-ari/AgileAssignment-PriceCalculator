8945a4291905368fba470a4c883f4d434d1bf919
Sowmya Ari <sowmya.ari@everest.engineering>
2021-04-25 22:27:50 +0530

    Add unit test case for total price calculation with given quantity and item price

--- src/main/java/PriceCalculator.java
+++ src/main/java/PriceCalculator.java
@@ -1,5 +1,9 @@
-public class PriceCalculator {
-    public static void main(String[] args){
 
+public class PriceCalculator {
+
+    public float calculatePriceWithoutTax(int quantity, float itemPrice){
+    }
+
+    public static void main(String[] args){
     }
 }
--- src/test/java/PriceCalculatorTest.java
+++ src/test/java/PriceCalculatorTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PriceCalculatorTest {
+    private PriceCalculator priceCalculator;
+
+    @Before
+    public void setUp(){
+        priceCalculator = new PriceCalculator();
+    }
+
+    @Test
+    public void testPriceCalculator () {
+        float itemPrice = 10;
+        int quantity = 5;
+        float totalPriceWithoutTax = priceCalculator.calculatePriceWithoutTax(quantity,itemPrice);
+        assertEquals(totalPriceWithoutTax,50);
+    }
 }
